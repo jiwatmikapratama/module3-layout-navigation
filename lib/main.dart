f5ea6afa4fd41b4aacf0bba0bbc9ee4837414176
jiwatmikapratama <jiwatmikapratama@gmail.com>
2023-01-18 03:45:55 +0800

    notif

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:module3_layout_and_navigation/layouts/login.dart';
 import 'package:responsive_framework/responsive_framework.dart';
-import 'dart:io';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
+// It is assumed that all messages contain a data field with the key 'type'
+Future<void> setupInteractedMessage() async {
+  // Get any messages which caused the application to open from
+  // a terminated state.
+  RemoteMessage? initialMessage =
+      await FirebaseMessaging.instance.getInitialMessage();
+
+  // If the message also contains a data property with a "type" of "chat",
+  // navigate to a chat screen
+  if (initialMessage != null) {
+    _handleMessage(initialMessage);
+  }
+
+  // Also handle any interaction when the app is in the background via a
+  // Stream listener
+  FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+}
+
+_launchURL() async {
+  if (!await launchUrl(
+    Uri.parse(
+        'https://www.google.com/'), //url ini bisa kalian ganti dengan url lain
+    mode: LaunchMode.externalApplication,
+  )) {
+    throw "Tidak bisa membuka halaman";
+  }
+}
+
+void _handleMessage(RemoteMessage message) {
+  if (message.notification!.title == 'pesan berhasil') {
+    _launchURL();
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(
     MaterialApp(
       builder: (context, child) => ResponsiveWrapper.builder(
